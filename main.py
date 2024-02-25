ea1085f8ba8f022116db88ef9bdeaa1418ee9840
Caprichoo <sandeepkb3899@outlook.com>
2024-02-25 22:19:54 +0530

    R&D phase.

--- main.py
+++ main.py
@@ -25,6 +25,12 @@ scissors = '''
 '''
 print("Welcome to Rock Paper Scissors")
 user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors\n"))
-
+if user_input == 0:
+    print(rock)
+elif user_input == 1:
+    print(paper)
+else:
+    print(scissors)
+print(f'Your input is {user_input}')
 #random.randint[]
 
