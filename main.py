da6183983511c31116aad82d342977f159f98eb2
Caprichoo <sandeepkb3899@outlook.com>
2024-02-25 22:15:21 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+rock = ''' 
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+print("Welcome to Rock Paper Scissors")
+user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors\n"))
+
+#random.randint[]
+
